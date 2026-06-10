60ebea22b2a1811f3367789b1b31ca4a541d9f8a
Alberto Conde Fernández <albertoconferri@gmail.com>
2026-06-10 16:05:02 +0200

    Delete Firebase de Google directory Para moverlo
    Moverlo

--- Firebase de Google/Practica3_AlbertoConde/Clase/app/src/main/java/com/example/clase/navigation/AppScreens.kt
+++
@@ -1,7 +0,0 @@
-package com.example.clase.navigation
-
-sealed class AppScreens (val route: String) {
-    object Resultados: AppScreens("Resultados")
-    object Formulario: AppScreens("Formulario")
-    object Inicio: AppScreens("Inicio")
-}
